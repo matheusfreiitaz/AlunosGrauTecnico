362b4b654bd9bbda4c46522a699cf983acad6a8e
Matheus Felipe <matheusfreiitas77@gmail.com>
2026-03-12 17:14:45 -0300

    Add files via upload

---
+++ arrays/EXEMPLO_2/script.js
@@ -0,0 +1,54 @@
+// Array que guarda as tarefas
+let tarefas = []
+
+function adicionarTarefa(){
+
+let input = document.getElementById("tarefa")
+let texto = input.value
+
+if(texto === ""){
+alert("Digite uma tarefa")
+return
+}
+
+// adiciona no array
+tarefas.push(texto)
+
+input.value = ""
+
+mostrarTarefas()
+
+}
+
+
+function mostrarTarefas(){
+
+let lista = document.getElementById("listaTarefas")
+
+lista.innerHTML = ""
+
+for(let i = 0; i < tarefas.length; i++){
+
+let li = document.createElement("li")
+
+li.innerHTML = `
+${tarefas[i]}
+<button class="remover" onclick="removerTarefa(${i})">
+X
+</button>
+`
+
+lista.appendChild(li)
+
+}
+
+}
+
+
+function removerTarefa(indice){
+
+tarefas.splice(indice,1)
+
+mostrarTarefas()
+
+}
